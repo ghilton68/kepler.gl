7036c57308620c44bc7a416632872290af9a876d
Shan He <shan@uber.com>
2019-02-15 15:45:14 -0800

    [Docs] Add ast script to build action -> updater tree
    - add jsDoc to actions and updaters
    
    - add action types, updater links to generated md
    
    - grouping actions, delete examples / parameters from toc

---
+++ scripts/ast-helper.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import {join} from 'path';
+
+export function walkSync(dir, fileList = []) {
+  dir.forEach(dir => {
+    const files = fs.readdirSync(dir);
+
+    files.forEach((file) => {
+      const path = join(dir, file);
+
+      if (fs.statSync(path).isDirectory()) {
+        fileList = walkSync(path, fileList);
+      } else if (path.endsWith('.js')){
+        fileList.push(path);
+      }
+    });
+  });
+
+  return fileList;
+};
